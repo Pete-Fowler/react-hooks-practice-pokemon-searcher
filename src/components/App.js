26b413afa4e82eaf9e9e2bfedaf932ef40beeb3c
Pete Fowler <fowler.pete@gmail.com>
2022-09-06 12:52:14 -0600

    Display images

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonPage from "./PokemonPage";
 
 function App() {
+  const [ list, setList ] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+    .then(res => res.json())
+    .then(data => setList(data))
+  }, [])
+
+  const listShown = list;
+
   return (
     <div className="App">
-      <PokemonPage />
+      <PokemonPage listShown={listShown}/>
     </div>
   );
 }
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,20 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ list }) {
+  
+  const cards = list.map(item => 
+  <PokemonCard 
+    key={item.id}
+    name={item.name} 
+    hp={item.hp} 
+    front={item.sprites.front}
+    back={item.sprites.back}
+  />);
+  
   return (
-    <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+    <Card.Group >  
+      {cards}
     </Card.Group>
   );
 }
